7a46a1bcc108d7b932b3b511a45214e399e523a7
♛    ∻Rom∻    ♚ <53869976+Romenildo@users.noreply.github.com>
2019-12-07 11:30:34 -0300

    implementado TAD

--- Organizador.h
+++ Organizador.h
@@ -1,1 +1,38 @@
-me
+#ifndef ORGANIZADOR_H_INCLUDED
+#define ORGANIZADOR_H_INCLUDED
+
+typedef struct organizador{
+    int MatriculaO;
+    char nome[30];
+    char cpf[15];
+    char telefone[15];
+    char email[30];
+};//ORGANIZADORES
+typedef struct organizador ORGANIZADOR;
+
+
+typedef struct capsulaO{
+    ORGANIZADOR dado;
+    struct capsulaO *proximo;
+};
+typedef struct capsulaO CAPSO;
+
+
+typedef struct listaO{
+    CAPSO *inicio;
+};
+typedef struct listaO LISTAO;
+
+LISTAO* CriarO();
+void liberarO(LISTAO* li);
+void mostrarTodosO(LISTAO* li);
+int tamanhoListaO(LISTAO *li);
+void inserirListaO(LISTAO *li,ORGANIZADOR eO);
+int mostrarEmRemoverO(CAPSO *aux);
+int removerListaO(LISTAO *li, int num);
+void pegarInfoOrganizador(ORGANIZADOR *eO, LISTAO *liO, int MatriculaO);
+void editarOrganizador(LISTAO *li, int matriculaINFO);
+
+
+#endif // ORGANIZADOR_H_INCLUDED
+
