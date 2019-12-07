c1045a5b3b1148cb873332e4a3477c2e711250ba
♛    ∻Rom∻    ♚ <53869976+Romenildo@users.noreply.github.com>
2019-12-07 11:31:29 -0300

    Update Visual.h

--- Visual.h
+++ Visual.h
@@ -1,21 +1,67 @@
 #ifndef VISUAL_H_INCLUDED
 #define VISUAL_H_INCLUDED
 
+void limparTela();
+gotoxy(int x,int y);
+PFchar(int ch);
+PMchar(int local1, int local2, int quant);
+Pchar(int local1, int local2, int caractere);
+PlinhaH(int tam);
+PlinhaV(int tam);
+linhaV(int tam, int local1, int local2);
+linhaH(int tam, int local1, int local2);
+VdesignOP();
+TelaLoading();
+Vcabecalho();
+VopcoesPrincipal();
+VopcoesPalestrantes();
+VopcoesCongressistas();
+VopcoesOrganizadores();
+VopcoesEventos();
+VopcoesEventosPalestras();
+VopcoesEventosGrupos();
+VopcoesEventosCursos();
+VopcoesEventosOficinas();
+Vcadastrar();
+VcadastrarCongressista();
+VcadastrarPalestra();
+VcadastrarGrupos();
+VcadastrarOficina();
 
-gotoxy(int x, int y);
+AvisoX();
+AvisoOP();
+AvisoEventoPalestra();
+AvisoEventPalestraLocais();
+AvisoEventoCursoLocais();
+AvisoEventoHorarios(char horas[7][10]);
+AvisoEventoLimpar();
+AvisoEventoGrupo();
+AvisoEventoCurso();
+AvisoEventoOficina();
+VeditarCongressista();
+VeditarPalestra();
+VeditarCurso();
+VremoverPalestra();
+VremoverGrupo();
+VremoverCurso();
+VremoverCongressista();
+VdesejaRemover();
+VeditadoComSucesso();
+AvisoNumeroNaoEncontrado();
+AvisoOPinvalida();
+AvisoVAZIO();
+AvisoADDsucesso();
+VcadastrarOrganizador();
+VeditarOrganizador();//pensar em substituir esse por um só e mudar somente o prinf
+VeditarGrupo();
+VremoverOrganizador();
+VcadastrarPalestrante();
+VeditarPalestrante();
+VremoverPalestrante();
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-#endif
+#endif // VISUAL_H_INCLUDED
