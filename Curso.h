dfad5cc62f1d588a3b603bd279e8b8eadfd57680
♛    ∻Rom∻    ♚ <53869976+Romenildo@users.noreply.github.com>
2019-12-10 00:01:49 -0300

    Finalizado!

--- Curso.h
+++ Curso.h
@@ -10,6 +10,8 @@ typedef struct eventoK{
     char tema[30];
     char palestrante[30];
     int EditH;
+    int CongN[50];
+    char CongNome[50][50]
 };//evento palestras
 typedef struct eventoK EVENTOK;
 
@@ -24,6 +26,8 @@ typedef struct listaK{
 };
 typedef struct listaK LISTAK;
 
+typedef struct listapT LISTApT;
+
 LISTAK* CriarK();
 void liberarK(LISTAK *li);
 void mostrarTodosK(LISTAK *li);
@@ -33,13 +37,16 @@ int mostrarEmRemoverK(CAPSK *aux);
 int removerListaK(LISTAK *li, int num);
 int PegarLocalCurso();
 int PegarHorarioCurso(char horas[7][10]);
-void pegarInfoCurso(EVENTOK *eK, LISTAK *liK, int n_cadastroK);
+void pegarInfoCurso(EVENTOK *eK, LISTAK *liK, int n_cadastroK,LISTApT *lipT);
 void EditarLocalCurso(CAPSK *aux);
 void EditarHorarioCurso(CAPSK *aux);
-void editarCurso(LISTAK *li, int cadastroINFO);
+void editarCurso(LISTAK *li, int cadastroINFO,LISTApT *lipT);
 
+void adicionarCongressistaK(LISTAK *li,int Curso,char *nome,int MAT);
+void MostrarCongressistaPorCurso(LISTAK *li,int Curso);
+void RemoverCongressistaDoCurso(LISTAK *li,int Curso,int Matricula);
 
-
-
+void desocuparPalestranteK(LISTApT *lipT,LISTAK *liK,int Curso);
+void removerHorarioK(LISTAK *li, int Curso);
 #endif // CURSOS_H_INCLUDED
 
